af63ac6c047d4989996d841bcf066e2c5e4422dc
Joseph Le <56007932+hungletram@users.noreply.github.com>
2020-02-05 15:34:05 +0700

    Add files via upload

--- bai1.py
+++ bai1.py
@@ -1,4 +1,5 @@
-# import datetim
+# Từ module datetime, mình import class datetime và timedelta để sử dụng những methods
+
 from datetime import datetime, timedelta
 
 
@@ -7,25 +8,37 @@ strtime0 = '2019-12-07T21:31:56.000Z'
 
 
 # khai báo biến fprice và gán cho nó giá trị bằng với strprice
-# fprice = <viết code của em ở đây >
-fprice = strprice
+
+# Remove dấu dollar rồi thay bằng chuỗi rỗng
+# Sau đó, chuổi rỗng sẽ được chuyển sang số thực
+fprice = float(strprice.replace("$", ""))
+
+# Thử phép tính giá trị thực nhân với 1.1
+fprice1 = fprice * 1.1
 
 # khai báo biến dttime0 và gán cho nó giá trị của stretime0
-# dttime0 = <viết code của em ở đây >
+
+# Sử dụng method strptime để chuyển thời gian dạng string sang dạng số
+# Method Strptime này có 2 arguments (value và format)
+# Format của strptime được chọn dựa trên tài liệu Python
 dttime0 = datetime.strptime(strtime0, "%Y-%m-%dT%H:%M:%S.000Z")
 
 # tạo biến dttime1 có giá trị bằng giá trị của dttime0 cộng thêm 1 giờ 2 phút 3 giây
-# dttime1 = <viết code của em ở đây >
+
+# Sử dụng timedelta method để cộng thêm thời gian, xuất ra giá trị mới dttime1
 dttime1 = dttime0 + timedelta(hours=1, minutes=2, seconds=3)
 
 # Xuất 3 giá trị đã tính ra màn hình
-# <viết code của em ở đây >
+
+# In giá trị của các biến
 print(fprice)
 print(dttime0)
 print(dttime1)
+print(f"{fprice1:.2f}")
 
 # Xuất kiểu (type) của 3 biến trên ra màn hình
-# <viết code của em ở đây >
+# In kiểu của các biến
 print(type(fprice))
+print(type(fprice1))
 print(type(dttime0))
 print(type(dttime1))
